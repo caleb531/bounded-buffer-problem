6df1ca58083eea6bbf6c0e4224d04c44040c1729
Caleb Evans <caleb@calebevans.me>
2017-04-11 14:42:42 -0700

    Solve cout synchronization issue via string streams

--- main.cpp
+++ main.cpp
@@ -42,7 +42,16 @@ void* produce(void *ptr) {
 		int insertStatus = buffer.insertItem(item);
 		// If the buffer is not full
 		if (insertStatus == 0) {
-			cout << "produce " << item << endl;
+			// Write the output to a separate stream and then cout the stream as
+			// one large string so as to avoid any synchronization issues with
+			// cout; even with the mutex lock/semaphore in place, the data may
+			// still be written to stdout after the lock is released, rendering
+			// the semaphore insufficient for making cout completely thread-safe
+			// (this has been confirmed by our own testing); see
+			// <http://stackoverflow.com/a/26909227/560642>
+			ostringstream ss;
+			ss << "produce " << item << endl;
+			cout << ss.str();
 		}
 
 		sem_post(&semMutex);
@@ -65,7 +74,9 @@ void* consume(void *ptr) {
 		int removeStatus = buffer.removeItem(item);
 		// If the buffer is not empty
 		if (removeStatus == 0) {
-			cout << "consume " << item << endl;
+			ostringstream ss;
+			ss << "consume " << item << endl;
+			cout << ss.str();
 		}
 
 		sem_post(&semMutex);
